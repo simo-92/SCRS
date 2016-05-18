ebe0fe3dd1e4db5f083815f0e99b24df78cf62b8
giordanocristini <giordano.cristini@gmail.com>
2016-05-18 16:10:39 +0200

    inizio implementazione dao

---
+++ Miner/src/main/java/it/scrs/miner/dao/BlockDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.scrs.miner.dao;
+
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author giordanocristini
+ */
+public class BlockDAO {
+    
+    
+}
